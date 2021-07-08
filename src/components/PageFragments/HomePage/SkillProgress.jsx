1145b3b1b7876405f4521af4010e6c26848e247c
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-07-08 10:25:03 +0900

    [about] skill layout and contents

--- src/components/PageFragments/HomePage/SkillProgress.jsx
+++ src/components/PageFragments/HomePage/SkillProgress.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import { Row, Col } from 'antd';
 import ProgressBar from '../../Progress';
+import style from './skill.module.less';
 
 const SkillsProgress = () => (
-  <div>
-    <h2>My Skills</h2>
-    <Row gutter={[20, 20]}>
-      <Col xs={24} sm={24} md={12}>
-        <ProgressBar percent={80} text="Javascript" />
-        <ProgressBar percent={95} text="ReactJS" />
-        <ProgressBar percent={85} text="Gatsby" />
-        <ProgressBar percent={90} text="NodeJS" />
-      </Col>
-      <Col xs={24} sm={24} md={12}>
-        <ProgressBar percent={70} text="Python" />
-        <ProgressBar percent={90} text="Mysql" />
-        <ProgressBar percent={78} text="MongoDB" />
-        <ProgressBar percent={80} text="Wordpress" />
-      </Col>
-    </Row>
-  </div>
+	<div className={`${style.skillSectionSpacing}`}>
+		<h2 className="titleSeparate">My Skills</h2>
+		<Row gutter={[20, 20]}>
+			<Col xs={24} sm={24} md={12}>
+				<ProgressBar percent={90} text="Javascript" mt="0px" />
+				<ProgressBar percent={85} text="Typescript" />
+				<ProgressBar percent={85} text="React" />
+				<ProgressBar percent={85} text="Redux" />
+				<ProgressBar percent={85} text="Jquery" />
+				<ProgressBar percent={70} text="NodeJS" />
+			</Col>
+			<Col xs={24} sm={24} md={12}>
+				<ProgressBar percent={85} text="Oracle" mt="0px" />
+				<ProgressBar percent={70} text="Java" />
+				<ProgressBar percent={50} text="K8S" />
+				<ProgressBar percent={50} text="ELK" />
+				<ProgressBar percent={50} text="Jenkins" />
+				<ProgressBar percent={50} text="DC/OS" />
+			</Col>
+		</Row>
+	</div>
 );
 
 export default SkillsProgress;
