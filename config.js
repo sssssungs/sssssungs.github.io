4efa31dbfd1acd3a90a6ccdc0156a23c4df0c277
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-07-11 13:21:55 +0900

    Applied google analytics

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ module.exports = {
 	contactFormUrl:
 		process.env.CONTACT_FORM_ENDPOINT ||
 		'https://getform.io/f/09a3066f-c638-40db-ad59-05e4ed71e451',
-	googleAnalyticTrackingId: process.env.GA_TRACKING_ID || '',
+	googleAnalyticTrackingId: process.env.GA_TRACKING_ID || 'G-1LBVTYLESG',
 	tags: {
 		javascript: {
 			name: 'javascript',
