862e74b5ceb01f2fec9ec23246ea0ef1dad72e16
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-08-11 18:54:02 +0900

    Changed web font for easing eyes

--- src/html.jsx
+++ src/html.jsx
@@ -14,14 +14,9 @@ export default class HTML extends React.Component {
 					<meta charSet="utf-8" />
 					<meta httpEquiv="x-ua-compatible" content="ie=edge" />
 					<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-					<link
-						href="https://fonts.googleapis.com/css2?family=Arvo&family=Mukta:wght@300&display=swap"
-						rel="stylesheet"
-					/>
 					{this.props.headComponents}
 				</head>
+
 				<body {...this.props.bodyAttributes} className="light">
 					{this.props.preBodyComponents}
 					<div key="body" id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
