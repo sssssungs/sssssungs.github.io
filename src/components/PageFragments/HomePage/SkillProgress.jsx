0ee07f934de6159a73fd653ded58d67b4103a38a
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-10-21 21:54:27 +0900

    reorder skills list

--- src/components/PageFragments/HomePage/SkillProgress.jsx
+++ src/components/PageFragments/HomePage/SkillProgress.jsx
@@ -9,19 +9,21 @@ const SkillsProgress = () => (
 		<Row gutter={[20, 20]}>
 			<Col xs={24} sm={24} md={12}>
 				<ProgressBar percent={90} text="Javascript" mt="0px" />
-				<ProgressBar percent={85} text="Typescript" />
 				<ProgressBar percent={85} text="React" />
-				<ProgressBar percent={85} text="Redux" />
 				<ProgressBar percent={85} text="Jquery" />
 				<ProgressBar percent={70} text="NodeJS" />
+				<ProgressBar percent={60} text="K8S" />
+				<ProgressBar percent={50} text="Graphql" />
+				<ProgressBar percent={40} text="DC/OS" />
 			</Col>
 			<Col xs={24} sm={24} md={12}>
-				<ProgressBar percent={85} text="Oracle" mt="0px" />
+				<ProgressBar percent={85} text="Typescript" mt="0px" />
+				<ProgressBar percent={85} text="Redux" />
+				<ProgressBar percent={85} text="Oracle" />
 				<ProgressBar percent={70} text="Java" />
-				<ProgressBar percent={50} text="K8S" />
 				<ProgressBar percent={50} text="ELK" />
-				<ProgressBar percent={50} text="Jenkins" />
-				<ProgressBar percent={50} text="DC/OS" />
+				<ProgressBar percent={40} text="Jenkins" />
+				<ProgressBar percent={40} text="Docker" />
 			</Col>
 		</Row>
 	</div>
