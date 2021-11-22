c6d099efa1baca895f468f2a41443a162c7916ac
sssssungs <32057999+sssssungs@users.noreply.github.com>
2021-11-22 19:21:44 +0900

    loading image

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout } from 'antd';
+import Lottie from 'react-lottie';
+import animationData from '../images/78259-loading.json';
 import Header from '../components/PageLayout/Header';
-
 import SidebarWrapper from '../components/PageLayout/Sidebar';
 import AboutMe from '../components/PageFragments/HomePage/AboutMe';
 import Skills from '../components/PageFragments/HomePage/SkillProgress';
 import ScrollHOC from './scrollHOC';
 
-export default () => (
-	<ScrollHOC>
-		<Layout className="outerPadding">
-			<Layout className="container">
-				<Header />
-				<SidebarWrapper>
-					<>
-						<AboutMe />
-						<Skills />
-					</>
-				</SidebarWrapper>
+export default () => {
+	const [loading, setLoading] = useState(true);
+	return (
+		<ScrollHOC>
+			<Layout className="outerPadding">
+				<Layout className="container">
+					<Header />
+					<SidebarWrapper>
+						<>
+							{loading ? (
+								<Lottie
+									options={{
+										animationData,
+										loop: 1,
+										autoplay: true,
+									}}
+									isStopped={false}
+									isPaused={false}
+									isClickToPauseDisabled={false}
+									eventListeners={[
+										{
+											eventName: 'complete',
+											callback: () => setLoading(false),
+										},
+									]}
+								/>
+							) : (
+								<div>
+									<AboutMe />
+									<Skills />
+								</div>
+							)}
+						</>
+					</SidebarWrapper>
+				</Layout>
 			</Layout>
-		</Layout>
-	</ScrollHOC>
-);
+		</ScrollHOC>
+	);
+};
