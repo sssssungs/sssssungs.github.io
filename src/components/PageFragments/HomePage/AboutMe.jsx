3ce2ee8308e47d032fa0016db0ee391e541c6037
sssssungs <32057999+sssssungs@users.noreply.github.com>
2022-01-15 20:40:57 +0900

    msa content

--- src/components/PageFragments/HomePage/AboutMe.jsx
+++ src/components/PageFragments/HomePage/AboutMe.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'antd';
-import AboutTile from '../../AbouTile';
+import AboutTile from '../../About';
 import { domHtml } from '../../../utils/stripTags';
 
 import SEO from '../../Seo';
