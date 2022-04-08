b0aba8c1275430cfc9487352b6a4842e0d5cac23
sssssungs <sssssungs@users.noreply.github.com>
2022-04-08 12:12:06 +0000

    deploy: 00e5717385943cd28dfb9bdfbf5cfc7bd1dcb5d3

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-dc2ba744540e5949fa68.js"
+    "url": "webpack-runtime-88198cb80079e4385a2a.js"
   },
   {
     "url": "framework-ac9b5dbfc53b5eded77d.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "17c5206fc2990145ece843f3085f653e"
+    "revision": "710dbfbf06d0128cfaa9055afc02877f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-780d0321bb4481125aff.js"
