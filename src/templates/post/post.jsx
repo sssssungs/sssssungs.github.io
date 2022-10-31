196b5ed23422b0f05cdd04dfb62732883fd26eb7
sssssungs <32057999+sssssungs@users.noreply.github.com>
2022-10-31 19:20:16 +0900

    mozarela font

--- src/templates/post/post.jsx
+++ src/templates/post/post.jsx
@@ -5,9 +5,6 @@ import Img from 'gatsby-image';
 import Header from '../../components/PageLayout/Header';
 import SidebarWrapper from '../../components/PageLayout/Sidebar';
 import SEO from '../../components/Seo';
-import Comment from '../../components/Comment';
-import Config from '../../../config';
-import Utils from '../../utils/pageUtils';
 
 import 'prismjs/themes/prism-solarizedlight.css';
 import './highlight-syntax.less';
@@ -24,8 +21,6 @@ const Post = ({ data }) => {
 		excerpt,
 		path,
 	} = frontmatter;
-
-	const canonicalUrl = Utils.resolvePageUrl(Config.siteUrl, Config.pathPrefix, path);
 
 	return (
 		<ScrollHOC>
@@ -54,7 +49,6 @@ const Post = ({ data }) => {
 								<Img className={style.bannerImg} fluid={fluid} title={excerpt} alt={title} />
 							</div>
 							<article className={style.blogArticle} dangerouslySetInnerHTML={{ __html: html }} />
-							<Comment pageCanonicalUrl={canonicalUrl} pageId={title} />
 						</div>
 					</SidebarWrapper>
 				</Layout>
