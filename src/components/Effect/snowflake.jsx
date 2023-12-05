d8afad29f22a689989a9580c277ccec97676de56
sssssungs <sssssungs@gmail.com>
2023-12-05 19:53:36 +0900

    snow effect

---
+++ src/components/Effect/snowflake.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Snowflake = () => (
+	<>
+		<div className="snowflake">❄️</div>
+		<div className="snowflake">❄️</div>
+		<div className="snowflake">❄️</div>
+		<div className="snowflake">❄️</div>
+		<div className="snowflake">❄️</div>
+		<div className="snowflake">❄️</div>
+		<div className="snowflake">❄️</div>
+		<div className="snowflake">❄️</div>
+		<div className="snowflake">❄️</div>
+		<div className="snowflake">❄️</div>
+		<div className="snowflake">❄️</div>
+		<div className="snowflake">❄️</div>
+	</>
+);
+
+export default Snowflake;
--- src/html.jsx
+++ src/html.jsx
@@ -5,6 +5,7 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import PropTypes from 'prop-types';
+import Snowflake from './components/Effect/snowflake';
 
 export default class HTML extends React.Component {
 	render() {
@@ -17,8 +18,8 @@ export default class HTML extends React.Component {
 					<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 					{this.props.headComponents}
 				</head>
-
 				<body {...this.props.bodyAttributes} className="light">
+					<Snowflake />
 					{this.props.preBodyComponents}
 					<div key="body" id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
 					{this.props.postBodyComponents}
