206b70674edf1548d31d030d58a6c1e80e8868c6
sssssungs <sssssungs@gmail.com>
2023-12-05 20:05:58 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-99c397edac56085e1f01.js"
+    "url": "webpack-runtime-318e9a24cf7558c542bc.js"
   },
   {
     "url": "framework-31fffcd60aca3c377820.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f6738e855076854485e46208101b692e"
+    "revision": "91e4310b35b40b6f7649be0c1bddb6a6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-f9cec80d358d469b76a9.js"
