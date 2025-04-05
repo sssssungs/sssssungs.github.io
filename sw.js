19649eae22ed00b83b7cc22c800a371094aeb865
sssssungs <sssssungs@gmail.com>
2025-04-05 13:08:03 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-edcdc8f14b5ed5ed2f78.js"
+    "url": "webpack-runtime-0c453893bac7b012acb7.js"
   },
   {
     "url": "framework-d9c8460637bd229d902b.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d02d6fd6c8707cf36b5ed122025425b2"
+    "revision": "ff80662602ab6a735daedfbc8cc517d1"
   },
   {
     "url": "manifest.webmanifest",
